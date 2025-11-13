5aa0b68fcc72902ac693ce909936bcff9b833a33
samfisher58 <symonislam58@gmail.com>
2025-11-13 20:57:31 +0600

    added navbar functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import { AuthContext } from '../../Context/AuthContext';
 
 const Navbar = () => {
 	const { user, logOut } = use(AuthContext);
+	console.log(user);
+
 	const handleLogOut = () => {
 		logOut()
 			.then(() => {
@@ -51,13 +53,12 @@ const Navbar = () => {
 							viewBox="0 0 24 24"
 							stroke="currentColor"
 						>
-							{' '}
 							<path
 								strokeLinecap="round"
 								strokeLinejoin="round"
 								strokeWidth="2"
 								d="M4 6h16M4 12h8m-8 6h16"
-							/>{' '}
+							/>
 						</svg>
 					</div>
 					<ul
@@ -71,14 +72,38 @@ const Navbar = () => {
 					Habit <span>Tracker</span>
 				</a>
 			</div>
+
 			<div className="navbar-center hidden lg:flex">
 				<ul className="menu menu-horizontal px-1">{links}</ul>
 			</div>
-			<div className="navbar-end">
+
+			<div className="navbar-end flex items-center gap-2">
 				{user ? (
-					<NavLink onClick={handleLogOut} className="btn">
-						Logout
-					</NavLink>
+					<>
+						<div className="dropdown dropdown-end">
+							<div
+								tabIndex={0}
+								role="button"
+								className="btn btn-ghost btn-circle avatar"
+							>
+								<div className="w-10 rounded-full">
+									<img alt="User" src={user.photoURL} />
+								</div>
+							</div>
+							<ul
+								tabIndex={0}
+								className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+							>
+								<li className="font-semibold">{user.displayName}</li>
+								<li className="text-sm text-gray-500">{user.email}</li>
+								<li>
+									<button onClick={handleLogOut} className="btn btn-sm mt-2">
+										Log out
+									</button>
+								</li>
+							</ul>
+						</div>
+					</>
 				) : (
 					<NavLink className="btn" to="/auth/login">
 						Sign In
